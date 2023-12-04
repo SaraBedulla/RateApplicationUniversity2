6d7d3b83388e5b61be06dd5861758109c59e703c
SaraBedulla <151458475+SaraBedulla@users.noreply.github.com>
2023-12-04 17:26:43 +0100

    Add files via upload

---
+++ server2/RateUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.example.rateuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateUniversityApplication.class, args);
+    }
+
+}
