33cb847a468b2ba22d7227df1a5397fe350783fe
enoklid <152066385+enoklid@users.noreply.github.com>
2024-01-11 17:26:05 +0100

    Add files via upload
    react workspace (footer + register component)

---
+++ react workspace (1)/front/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>copyrights reserved</p>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
