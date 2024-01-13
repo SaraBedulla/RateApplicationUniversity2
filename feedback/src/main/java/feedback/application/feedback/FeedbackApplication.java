6359dd8e7be98432ef368c51cccc4fb9b108c95e
doriskole <koledoris18@gmail.com>
2024-01-13 19:54:15 +0100

    Add existing project files to Git

---
+++ feedback/src/main/java/feedback/application/feedback/FeedbackApplication.java
@@ -0,0 +1,13 @@
+package feedback.application.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackApplication.class, args);
+	}
+
+}
---
+++ feedback/src/test/java/feedback/application/feedback/FeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package feedback.application.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
