cf46edece6c247e8b0193d849fd21c90d1f5d381
anisafrasheri <anisa.frasheri@fti.edu.al>
2024-01-14 17:06:18 +0100

    Add CourseService and FeedbackService files under Service

---
+++ feedback/src/main/java/feedback/application/feedback/service/CourseService.java
@@ -0,0 +1,40 @@
+package feedback.application.feedback.service;
+
+import feedback.application.feedback.model.Course;
+import feedback.application.feedback.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> findAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Optional<Course> findCourseById(Long id) {
+        return courseRepository.findById(id);
+    }
+
+    public Course saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+
+
+
+}
+
---
+++ feedback/src/main/java/feedback/application/feedback/service/FeedbackService.java
@@ -0,0 +1,38 @@
+package feedback.application.feedback.service;
+
+import feedback.application.feedback.model.Feedback;
+import feedback.application.feedback.repository.FeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FeedbackService {
+
+    private final FeedbackRepository feedbackRepository;
+
+    @Autowired
+    public FeedbackService(FeedbackRepository feedbackRepository) {
+        this.feedbackRepository = feedbackRepository;
+    }
+
+    public List<Feedback> findAllFeedbacks() {
+        return feedbackRepository.findAll();
+    }
+
+    public Optional<Feedback> findFeedbackById(Long id) {
+        return feedbackRepository.findById(id);
+    }
+
+    public Feedback saveFeedback(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    public void deleteFeedback(Long id) {
+        feedbackRepository.deleteById(id);
+    }
+
+}
+
