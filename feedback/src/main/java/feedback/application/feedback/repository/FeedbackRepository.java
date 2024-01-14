3b7831af0219fa92e4ca190cb07c3fd4bb42fcc3
doriskole <koledoris18@gmail.com>
2024-01-14 14:24:34 +0100

    Adding 'Feedback' class&interface to model and repo

---
+++ feedback/src/main/java/feedback/application/feedback/repository/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package feedback.application.feedback.repository;
+
+
+import feedback.application.feedback.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+}
