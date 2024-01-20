d23b571f3f527bea78527bcc5bc2aea882ee8ef4
SaraBedulla <151458475+SaraBedulla@users.noreply.github.com>
2024-01-20 13:55:32 +0100

    FINAL

--- feedback/src/main/java/feedback/application/feedback/model/Student.java
+++ feedback/src/main/java/feedback/application/feedback/model/Student.java
@@ -43,11 +43,17 @@ public class Student {
         this.id = id;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
